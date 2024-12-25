696c8d46b78640f5f73ace48234a1ebf38d10d00
Dongho Kim <dongho7559@naver.com>
2024-12-25 19:31:27 +0900

    [Silver V] Title: 나이순 정렬, Time: 312 ms, Memory: 49484 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,18 +1,10 @@
-N = int(input())
-member_sort = []
+import sys
 
-for i in range(N):
-    age, member = input().split()
-    age = int(age)
-    member_sort.append([age, member])
-    
 def quick_sort_age(arr):
     if(len(arr) <= 1):
         return arr
-    
     pivot = arr[len(arr)//2][0] #2차원리스트에서 0번째 인덱스인 age기준으로 정렬    
     left, equal, right = [], [], []
-    
     for num in arr:
         if( num[0] < pivot):
             left.append(num)
@@ -20,9 +12,16 @@ def quick_sort_age(arr):
             right.append(num)
         else:
             equal.append(num)
-    
     return quick_sort_age(left) + equal + quick_sort_age(right)
 
+N = int(sys.stdin.readline())
+member_sort = []
+
+for i in range(N):
+    age, member = sys.stdin.readline().split()
+    age = int(age)
+    member_sort.append([age, member])
+    
 member_sort = quick_sort_age(member_sort)
 
 for i in member_sort:
