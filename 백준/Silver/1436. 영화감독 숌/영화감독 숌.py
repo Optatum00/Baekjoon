111ea810ab18a17126aec738515ea3282ea889e7
Dongho Kim <dongho7559@naver.com>
2024-12-31 21:37:36 +0900

    [Silver V] Title: 영화감독 숌, Time: 692 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -3,13 +3,13 @@ import sys
 N = int(sys.stdin.readline().rstrip())
 
 count = 0
-target = '666'
+target = 665
 while True:
-    if ('666' in target  ):
-        count += 1 
-    if (N == count):
-        break
-    target = int(target)
     target += 1
-    target = str(target)
+    x = str(target)
+    if ('666' in x):
+        count += 1 
+        if (N == count):
+            break
+
 print(target)
