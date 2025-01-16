adf7d76c4e4b40df7f887553760e32f27131515c
Dongho Kim <dongho7559@naver.com>
2025-01-17 02:35:56 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -8,12 +8,14 @@ for i in range(N):
     line = sys.stdin.readline().strip()
     board.append(line)
 
-def get_min(board):
+# 8X8 체스판에서 첫색깔이 W,B일때 중 고칠거 적은거를 택함
+def get_min(board): 
     test1, test2 = "WBWBWBWB", "BWBWBWBW"
     res = min(check(board,test1,test2), check(board,test2,test1))
     return res
 
-def check(board,first_test,second_test):
+# test1 test2를 직접 설정해서 이에 맞게 하나씩 테스트함
+def check(board,first_test,second_test): 
     count = 0
     for i in range(0,8,2):
         for j in range(8):
@@ -31,6 +33,7 @@ def check(board,first_test,second_test):
                 count += 1
     return count
 
+# MxN 보드를 8x8로 슬라이싱
 def slice_board(board):
     initial_board = board
     sliced_board = []
@@ -39,11 +42,13 @@ def slice_board(board):
     length_case = N-8+1
     for i in range(width_case):
         for j in range(length_case):
-            # 위아래로 8개 맞추기
+            # 위아래로 8개 맞추기싱 (슬라이싱을 한다고 바로 수정이 안되기에 updown에 저장)
             updown_board = initial_board[j:8+j]
-            # 양옆으로 8개 맞추기
+            # 양옆으로 8개 맞추기 (저장된 updown에서 가로로 8개씩 슬라이싱)
             for k in range(8):
                 sliced_board.append(updown_board[k][i:8+i])
+            # -> initial_board = [row[j:j + 8] for row in board[i:i + 8]]
+            # -> 위에 슬라이싱 한줄로 단축가능 by GPT피셜
             # 케이스별로 오류갯수 찾고 갱신
             error = get_min(sliced_board)
             if (res > error):
