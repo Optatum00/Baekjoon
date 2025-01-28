cd511b6ae1e4685df189406b09c9b7b17f0d7429
Dongho Kim <dongho7559@naver.com>
2025-01-28 22:55:15 +0900

    [Silver III] Title: 소수 구하기, Time: 3320 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,14 @@
+M, N = map(int, input().split())
+
+def isPrime(num):
+    if num == 1:
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if (num % i == 0):
+            return False
+    else:
+        return True
+    
+for i in range(M, N+1):
+    if isPrime(i):
+        print(i)
