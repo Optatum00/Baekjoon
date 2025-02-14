c1949c06d01a343c1f7346d252a42c375a38c9b7
Dongho Kim <dongho7559@naver.com>
2025-02-15 02:11:05 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().strip().split())
+
+#dp 이용한 파스칼 삼각형
+def comb_pascal(n, r):
+    vector = [1 for _ in range(r+1)]
+    for _ in range(1, n - r + 1):
+        for j in range(1, r + 1):
+            vector[j] = vector[j-1] + vector[j]
+    return vector[r]
+
+print(comb_pascal(N-1,K-1))
