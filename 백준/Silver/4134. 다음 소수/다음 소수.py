5795a9cdc0cdf90ea1ca98fda55d3b9c38fca2d3
Dongho Kim <dongho7559@naver.com>
2025-02-20 15:36:34 +0900

    [Silver IV] Title: 다음 소수, Time: 384 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.py
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -2,10 +2,7 @@ import sys
 input = sys.stdin.readline
 
 def isPrime(num):
-    if num == 1:
-        return False
-    max = int(num**0.5)+1
-    for i in range(2, max):
+    for i in range(2, int(num**0.5)+1):
         if (num % i == 0):
             return False
     else:
@@ -16,7 +13,7 @@ for i in range(n):
     M = int(input().strip())
     i = M   
     while (True):
-        if (M == 0 or M == 1 or M == 2):
+        if (M == 0 or M == 1):
             print(2)
             break
         if isPrime(i):
